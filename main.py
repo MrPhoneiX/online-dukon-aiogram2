8e22cfe6e97d30d9580ab03622936d6e5d3e524e
MrPhoneiX <100272193+MrPhoneiX@users.noreply.github.com>
2024-05-19 15:00:45 +0500

    boshlanishi

---
+++ main.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+bot = Bot(os.getenv('TOKEN'))
+dp = Dispatcher(bot=bot)
+
+
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    await message.answer(f'{message.from_user.first_name}, Xush keldingiz!')
+
+
+@dp.message_handler()
+async def answer(message: types.Message):
+    await message.reply('Uzr, sizni tushunmadim!')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
